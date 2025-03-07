f9cb8f2f58c432cd8d98edc7be13f22535d95392
DenisBelikov <DenisBelikov1992@yandex.ru>
2025-03-07 13:13:47 +0300

    testing

--- tests/test_trial.py
+++ tests/test_trial.py
@@ -24,3 +24,21 @@ class TestNews(TestCase):
         news_count = News.objects.count()
         # Сравним полученное число с единицей.
         self.assertEqual(news_count, 1)
+
+
+class TestNews(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.news = News.objects.create(
+            title='Заголовок новости',
+            text='Тестовый текст',
+        )
+
+    def test_successful_creation(self):
+        news_count = News.objects.count()
+        self.assertEqual(news_count, 1)
+
+    def test_title(self):
+        # Сравним свойство объекта и ожидаемое значение.
+        self.assertEqual(self.news.title, 'Заголовок новости')
